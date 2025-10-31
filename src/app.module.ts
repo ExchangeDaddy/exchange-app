47353a0b8d35811ca4bd98b1228de6592ea39a5d
ExchangeDaddy <exchanger142@gmail.com>
2025-10-31 18:42:14 +0200

    db-connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') === 'prod';
+        if (!isProduction) Logger.warn('DB_URL:', configService.get('DB_URL'));
+
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: false,
+          url: configService.get('DB_URL'),
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
